b6d4e71e935abf21d83f3ffe3cb55f9d327151aa
JulCR34l <davidrealcastiblanco@gmail.com>
2026-03-11 22:46:29 -0500

    feat:(backend): Security improve

---
+++ src/main/java/com/aguardientes/azarcafetero/user_service/infrastructure/security/JwtService.java
@@ -0,0 +1,31 @@
+package com.aguardientes.azarcafetero.user_service.infrastructure.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+
+@Service
+public class JwtService {
+
+    private final SecretKey key;
+
+    public JwtService(@Value("${jwt.secret}") String secret) {
+        this.key = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public String extractUserId(String token) {
+
+        Claims claims = Jwts.parser()
+                .verifyWith(key)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+
+        return claims.getSubject();
+    }
+}
