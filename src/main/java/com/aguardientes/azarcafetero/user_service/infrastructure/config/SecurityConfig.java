dae47e5d3c3ff8abbd694bae3bdec5236b276c9c
JulCR34l <davidrealcastiblanco@gmail.com>
2026-03-11 22:46:29 -0500

    feat:(backend): Security improve

--- src/main/java/com/aguardientes/azarcafetero/user_service/infrastructure/config/SecurityConfig.java
+++ src/main/java/com/aguardientes/azarcafetero/user_service/infrastructure/config/SecurityConfig.java
@@ -1,22 +1,54 @@
 package com.aguardientes.azarcafetero.user_service.infrastructure.config;
 
+import com.aguardientes.azarcafetero.user_service.infrastructure.security.JwtFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
 
+    private final JwtFilter jwtFilter;
+
+    public SecurityConfig(JwtFilter jwtFilter) {
+        this.jwtFilter = jwtFilter;
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
         http
                 .csrf(csrf -> csrf.disable())
+
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/profile/**").permitAll()
-                        .requestMatchers("/swagger-ui/**", "/api-docs/**", "/swagger-ui.html").permitAll()  // ← agrega esta línea
+
+                        // swagger
+                        .requestMatchers(
+                                "/swagger-ui/**",
+                                "/v3/api-docs/**",
+                                "/swagger-ui.html"
+                        ).permitAll()
+
+                        // endpoints públicos
+                        .requestMatchers("/profile/status").permitAll()
+
+                        // endpoints protegidos
+                        .requestMatchers("/profile/**").authenticated()
+
                         .anyRequest().permitAll()
                 )
+
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+
                 .httpBasic(basic -> basic.disable())
                 .formLogin(form -> form.disable());
 
